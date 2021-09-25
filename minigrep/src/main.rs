991aeecf0329adf164f6f4f9e9f0d0504df51798
Mihango <mihangokennedy@gmail.com>
2021-09-25 09:52:33 +0300

    added case sensitivity to searching

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,26 +1,25 @@
 #![allow(unused)]
 
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     // get arguments using std::args function which returns an iterator
     // call iterator's collect method turns the data produced to a collection
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
-    println!("Searching for {}", query);
-    println!("In file {} \n", filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parging arguments: {}", err);
+        process::exit(1);
+    });
 
-    let contents =
-        fs::read_to_string(filename).expect("Something went wrong while reading the file");
+    println!("Searching for {}", config.query);
+    println!("In file {} \n", config.filename);
 
-    println!("With text:\n{}", contents);
-}
-
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-
-    (query, filename)
+    if let Err(er) = minigrep::run(config) {
+        eprintln!("Application error: {}", er);
+        process::exit(1);
+    }
 }
